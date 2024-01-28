315dea5870d8a3b4560646a77bfce634873e20eb
Amisha PRIYA <priyaamisha03@gmail.com>
2024-01-28 12:09:01 +0530

    Display input field for name upon clicking edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditButtonClick() {
+        setIsEditing(true);
+    }
+
     return (
         <>
             <li>
                 <span className="player">
-                    <span className="player-name">{name}</span>
+                    {isEditing
+                    ? <input type="text" required></input>
+                    : <span className="player-name">{name}</span>}
                     <span className="player-symbol">{symbol}</span>
                 </span>
-                <button>Edit</button>
+                <button onClick={handleEditButtonClick}>Edit</button>
             </li>
         </>
     )
