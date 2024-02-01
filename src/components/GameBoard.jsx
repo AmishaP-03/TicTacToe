ea5cdc0007675c2acd1c36f4090222fe3b224d45
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-01 19:47:19 +0530

    Adding click action on button + updating object state immutably

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,27 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleUpdate(rowIndex, colIndex) {
+        // We have to update the game board while making sure not to lose the previous state.
+        setGameBoard((previousGameBoard) => {
+            /**
+             * If our state is an object (which includes arrays as well), it should not be updated directly. Rather it should be updated in an immutable way.
+             * This implies that the concerned state object should not be modified directly. Instead, we should first create its copy and update it.
+             * This is because if we directly update the state object (previousGameBoard) here, then we will cause the old value in memory to update even before this scheduled state update  is executed by React.
+             */
+            const updatedBoard = [...previousGameBoard.map((innerArray) => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        })
+    }
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleUpdate(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
