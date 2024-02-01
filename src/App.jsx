1a801ceffb186510f8cdc39f280faf3493e41c48
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-01 12:13:26 +0530

    Make the name fields editable and add the logic to save them

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ function App() {
         <main>
             <div id="game-container">
                 <ol id="players">
-                    <Player name="Player 1" symbol="X"/>
-                    <Player name="Player 2" symbol="0"/>
+                    {/* A very powerful feature of React - build super complex reusable components which do not interfere with each other: */}
+                    {/* React creates one isolated instance of component Player */}
+                    <Player initialName="Player 1" symbol="X"/>
+                    {/* React creates another isolated instance of component Player */}
+                    {/* Thus, we we click on edit button against a player, it opens up an input box only for that specific player */}
+                    <Player initialName="Player 2" symbol="0"/>
                 </ol>
                 Game board
             </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react"
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
     const [isEditing, setIsEditing] = useState(false);
+    const [currentPlayerName, setPlayerName] = useState(initialName);
 
     function handleEditButtonClick() {
-        setIsEditing(true);
+        setIsEditing(editing => !editing);
+    }
+
+    // React gives us an event object as argument when onChange is triggered
+    function handleNameChange(event) {
+        // event.target -> element that triggered this event, here 'input' element
+        // event.target.value -> input element has a value propert which holds the data entered by user in the input field
+        setPlayerName(event.target.value);
     }
 
     return (
@@ -12,11 +20,13 @@ export default function Player({name, symbol}) {
             <li>
                 <span className="player">
                     {isEditing
-                    ? <input type="text" required></input>
-                    : <span className="player-name">{name}</span>}
+                    ? <input type="text" required value={currentPlayerName} onChange={handleNameChange}></input> // 2 way binding mechanism. Listening to change in input and updating it back to input value
+                    // Post state update through function handleNameChange, the value prop will get updated with the latest player name
+                    // Setting value of onChange prop makes the input field editable. Else, it would not have been possible to change the input value, since currentPlayerName was never updated
+                    : <span className="player-name">{currentPlayerName}</span>}
                     <span className="player-symbol">{symbol}</span>
                 </span>
-                <button onClick={handleEditButtonClick}>Edit</button>
+                <button onClick={handleEditButtonClick}>{isEditing ? 'Save': 'Edit'}</button>
             </li>
         </>
     )
