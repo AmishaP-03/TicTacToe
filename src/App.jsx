92808a0279be6fbfef2201d9c5344538e2f44c25
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-02 16:06:06 +0530

    Deriving the gameBoard state from a state in App components

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,28 @@ function App() {
     // Player with symbol X is the default active player
     const [activePlayer, setActivePlayer] = useState('X');
 
+    // Game turns info (array of objects, each object will contain information like: a) the row and column indices of the square selected b) the player symbol which selected that square)
+    const [gameTurns, setGameTurns] = useState([]);
+
     // Switch turns when a square is selected
-    function handleSelectSquare() {
+    function handleSelectSquare(rowIndex, colIndex) {
         setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+        setGameTurns((currentTurns) => {
+            // We cannot depend on the other state to get the value of activePlayer as it might not be the updated value. So we'll compute the value of active player here.
+            // Trick to avoid merging of two states
+            let currentPlayer = 'X';
+            if (!!currentTurns.length && currentTurns[0].player === 'X') {
+                currentPlayer = 'O';
+            }
+            const updatedTurns = [
+                {
+                    square: {row: rowIndex, col: colIndex},
+                    player: currentPlayer
+                },
+                ...currentTurns
+            ];
+            return updatedTurns;
+        });
     }
     return (
         <main>
@@ -21,7 +40,8 @@ function App() {
                     {/* Thus, we we click on edit button against a player, it opens up an input box only for that specific player */}
                     <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
                 </ol>
-                <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+                {/* <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/> */}
+                <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
             </div>
         </main>
     );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,36 +1,54 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+// export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
+export default function GameBoard({onSelectSquare, turns}) {
+    // LIFTING THIS STATE UP TO THE APP COMPONENT SO THAT IS CAN BE REUSED BY LOG COMPONENT AS WELL
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        // Handle the player turn switch logic in the parent
-        onSelectSquare();
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-        // We have to update the game board while making sure not to lose the previous state.
-        setGameBoard((previousGameBoard) => {
-            /**
-             * If our state is an object (which includes arrays as well), it should not be updated directly. Rather it should be updated in an immutable way.
-             * This implies that the concerned state object should not be modified directly. Instead, we should first create its copy and update it.
-             * This is because if we directly update the state object (previousGameBoard) here, then we will cause the old value in memory to update even before this scheduled state update  is executed by React.
-             */
-            const updatedBoard = [...previousGameBoard.map((innerArray) => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     // Handle the player turn switch logic in the parent
+    //     onSelectSquare();
+
+    //     // We have to update the game board while making sure not to lose the previous state.
+    //     setGameBoard((previousGameBoard) => {
+    //         /**
+    //          * If our state is an object (which includes arrays as well), it should not be updated directly. Rather it should be updated in an immutable way.
+    //          * This implies that the concerned state object should not be modified directly. Instead, we should first create its copy and update it.
+    //          * This is because if we directly update the state object (previousGameBoard) here, then we will cause the old value in memory to update even before this scheduled state update  is executed by React.
+    //          */
+    //         const updatedBoard = [...previousGameBoard.map((innerArray) => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    // }
+
+    // Build the game board from turns prop
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        // Object destructuring, should have the same name as defined in turn object
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        // We need not manage any other state here to update the game board
+        // Thus gameboard is a derived state here, value computed from some other state
+        // AIM: MANAGE AS LESS STATES AS POSSIBLE. REUSE EXISTING STATES TO THE MAX POSSIBLE EXTENT
+        gameBoard[row][col] = player;
+        console.log(gameBoard);
     }
+
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        {/* <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button> */}
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
